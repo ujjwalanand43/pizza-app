ba75eeac54423e6c0fde37bb27ebbd44b8a26838
ujjwalanand <ujjwalanand4277@gmail.com>
2021-06-16 20:11:31 +0530

    commit

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ const app = express();
 const PORT = process.env.PORT||80
 let ejs = require('ejs');
 const path = require('path')
-// const expressLayout = require('express-ejs-layouts')
+var expressLayouts = require('express-ejs-layouts');
 // setting the template engine
-// app.use(expressLayout)
+app.use(expressLayouts)
 // public
  app.use(express.static('public'))
 // console.log(static)
 app.set('views',path.join(__dirname,'./resources/views'))
-app.set('views',path.join(__dirname,'./resources/views/customers'))
 app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
     res.render('index')
 })
 app.get('/cart',(req,res)=>{
-    res.render('cart')
+    res.render('customers/cart')
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
 })
 
 app.listen(PORT,()=>{
